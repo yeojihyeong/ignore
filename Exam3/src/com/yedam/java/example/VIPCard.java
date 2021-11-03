a1e510649dbc2d2e753f8022e4f4b75ff0c46e0f
admin <admin@DESKTOP-G41G93U>
2021-11-03 17:48:19 +0900

    1

--- Exam3/src/com/yedam/java/example/VIPCard.java
+++ Exam3/src/com/yedam/java/example/VIPCard.java
@@ -14,4 +14,13 @@ public class VIPCard implements Payment {
 		this.pointRatio = 0.03;
 	}
 
+	@Override
+	public void showCardInfo() {
+		System.out.println("카드명 : " + name);
+		System.out.println("적용등급 : " + grade);
+		System.out.println("할인율 : " );
+		System.out.println("포인트 적립율 : " + pointRatio);
+		System.out.println("적립 포인트 :" + point);
+	}
+
 }
