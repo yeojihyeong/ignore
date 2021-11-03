d620c6c4a0fbfae3cbc7a1070706fea77e145efc
admin <admin@DESKTOP-G41G93U>
2021-11-03 14:09:35 +0900

    1

---
+++ ch11/src/com/yedam/java/ch05/stringtoken/Main.java
@@ -0,0 +1,14 @@
+package com.yedam.java.ch05.stringtoken;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// String.split() : 정규표현식 
+		// StringTokenizer: 문자
+		// 공통점 : 문자열이 특정부분자로 있어 특정 부분자로 분리할수잇을때 ex) 특수문자
+		// 010-123-1234 : -, 010  123  1234 => 문자열로 분리
+		
+		
+	}
+
+}
---
+++ ch11/src/com/yedam/java/ch05/stringtoken/Regex.java
@@ -0,0 +1,21 @@
+package com.yedam.java.ch05.stringtoken;
+
+public class Regex {
+	/*
+	 * [] 한개의 문자
+	 * 		1) [abc] -> a or b or c
+	 * 		2) [^abc] -> a,b,c 이외의 하나의 문자
+	 * 		3) [a-zA-Z] -> a~z A~Z 중 하나
+	 * 
+	 *  \d 한개의 숫자로 [0-9]와 동일
+	 *  \s 공백
+	 *  \w 한개의 알파벳 또는 한개의 숫자, [a-zA-Z0-9]와 동일
+	 *  ? 없음 또는 한개
+	 *  * 없음 또는 한개 이상
+	 *  + 한개이상
+	 *  {n} 정확히 n개
+	 *  {n,} 최소 n개 이상
+	 *  {n,m} n개에서  m개 까지
+	 *  () 그룹핑 
+	 */
+}
